b0fea4a6d8245f0bd817a2e5ba7cdb6b14f336f3
Dinesh Tamang <dineshtamang7263@gmail.com>
2023-02-12 11:58:34 +0530

    created Packet filter feature

--- Packet_Sniffer/main.py
+++ Packet_Sniffer/main.py
@@ -3,13 +3,20 @@ from scapy.layers import http
 
 
 def sniff(interface: str) -> None:
+    '''Takes interface to sniff data'''
     scapy.sniff(iface=interface, store=False, prn=process_sniffed_packet)
     
     
 def process_sniffed_packet(packet) -> None:
+    '''Takes Packets and Prints it'''
     print(type(packet))
+    # filtering a data from packets
     if packet.haslayer(http.HTTPRequest):
-        print(packet)
+        if packet.haslayer(scapy.Raw):
+            # printing a data from all layers.
+            # print(packet.show()) 
+            # printing a specific field from packet
+            print(packet[scapy.Raw].load)
     
     
 sniff("eth0")
