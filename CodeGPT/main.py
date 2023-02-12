d28a537a808058f0dc182d766e8dc089f1eaed6a
Dinesh Tamang <dineshtamang7263@gmail.com>
2023-02-12 15:00:26 +0530

    code refactor

--- CodeGPT/main.py
+++ CodeGPT/main.py
@@ -1,18 +1,27 @@
 import openai
+import os
 
 
-openai.api_key = ""
-model_engine = "text-davinci-002"
-prompt = str(input("Enter your Question: "))
 
-completion = openai.Completion.create(
+def get_response(model_engine, prompt):
+    '''Takes prompt as a input and returns a responses'''
+    completion = openai.Completion.create(
         engine = model_engine,
         prompt=prompt,
         max_tokens = 1024,
         n = 1,
         stop = None,
         temperature = 0.9,
-        )
+    )
+    
+    return completion.choices[0].text
 
-response = completion.choices[0].text
+        
+value = os.getenv("apiKey", default=None)
+openai.api_key = value
+model_engine = "text-davinci-002"
+prompt = str(input("Enter your Question: "))
+
+
+response = get_response(model_engine, prompt)
 print(response)
